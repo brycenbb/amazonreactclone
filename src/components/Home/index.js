13881fbf311c78e267af044826645feb07870da7
brycen <brycenbb1996@gmail.com>
2022-07-14 09:07:51 +0100

    made custom CSS files!

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,10 +1,12 @@
 //Has hero that is 100% width and cards rows that are 90%
 import Header from '../Header';
 import Recs from '../Recommendations';
+import Banner from '../Banner';
 function Home() {
   return (
     <>
       <Header></Header>
+      <Banner></Banner>
       <Recs></Recs>
     </>
   );
