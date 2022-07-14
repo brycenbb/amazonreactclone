d34ceecbb9650095221a6aa1609312dd5381c1cc
brycen <brycensoc@gmail.com>
2022-07-14 09:30:02 +0100

    added first image to page, need to format and then allow for overlaying the cards on the bottom of the image then add other images

--- src/components/Recommendations/index.js
+++ src/components/Recommendations/index.js
@@ -1,23 +1,31 @@
 import cardList from './cardsData';
 import ActionAreaCard from '../Card';
 import './index.css';
+import img from '../../assests/amazon1.jpg';
 function Recs() {
   return (
     <main id="recs">
-      {cardList.map((item, index) => {
-        return (
-          // <div key={index} className="card">
-          //   <h3>{item.title}</h3>
-          //   <img src={item.pics[0]} alt=""></img>
-          //   <div className="seeMore"> See More </div>
-          // </div>
-          <ActionAreaCard
-            key={index}
-            title={item.title}
-            picture={item.pics[0]}
-          ></ActionAreaCard>
-        );
-      })}
+      <section>
+        <div className="bannerImageContainer">
+          <img className="croppedImage" src={img}></img>
+        </div>
+      </section>
+      <section className="cardSection">
+        {cardList.map((item, index) => {
+          return (
+            // <div key={index} className="card">
+            //   <h3>{item.title}</h3>
+            //   <img src={item.pics[0]} alt=""></img>
+            //   <div className="seeMore"> See More </div>
+            // </div>
+            <ActionAreaCard
+              key={index}
+              title={item.title}
+              picture={item.pics[0]}
+            ></ActionAreaCard>
+          );
+        })}
+      </section>
     </main>
   );
 }
