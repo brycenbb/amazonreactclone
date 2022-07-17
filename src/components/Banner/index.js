1fe796e6f5e6475fecfee29f00d1ab99a086f6c2
brycen <brycenbb1996@gmail.com>
2022-07-17 14:21:01 +0100

    banner done - no fade

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,5 +1,26 @@
+import { useState } from 'react';
+import img1 from '../../assests/amazon1.jpg';
+import img2 from '../../assests/amazon2.jpg';
+import img3 from '../../assests/amazon3.jpg';
+import img4 from '../../assests/amazon4.jpg';
+import img5 from '../../assests/amazon5.jpg';
+import './index.css';
 function Banner() {
-  return <div></div>;
+  let imageArr = [img1, img2, img3, img4, img5];
+  const [currImage, nextImage] = useState(1);
+  function handleClick() {
+    nextImage((currImage) => (currImage + 1) % 5);
+  }
+  return (
+    <div id="imageContainer">
+      {' '}
+      <img
+        className="bannerImgs"
+        src={imageArr[currImage]}
+        onClick={handleClick}
+      ></img>
+    </div>
+  );
 }
 
 export default Banner;
--- src/components/Recommendations/index.js
+++ src/components/Recommendations/index.js
@@ -1,15 +1,11 @@
 import cardList from './cardsData';
 import ActionAreaCard from '../Card';
+import Banner from '../Banner';
 import './index.css';
-import img from '../../assests/amazon1.jpg';
+
 function Recs() {
   return (
     <main id="recs">
-      <section>
-        <div className="bannerImageContainer">
-          <img className="croppedImage" src={img}></img>
-        </div>
-      </section>
       <section className="cardSection">
         {cardList.map((item, index) => {
           return (
