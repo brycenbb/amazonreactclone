fc9496ee23b78222b162d87d85862a733668bb1e
brycen <brycenbb1996@gmail.com>
2022-07-22 19:08:50 +0100

    ready to use the dandelion api

--- src/components/SearchResults/index.js
+++ src/components/SearchResults/index.js
@@ -5,6 +5,7 @@ import products from './results';
 import { v4 as uuidv4 } from 'uuid';
 function SearchResults() {
   const { searchTerm } = useContext(searchContext);
+
   //will fetch data on what was searched
   // return <div className="SR"> You searched for {searchTerm}</div>;
 
