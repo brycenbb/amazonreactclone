b41fc2b3e3d263c776b7e618c43a6701d50433d7
brycen <brycensoc@gmail.com>
2022-07-22 16:26:34 +0100

    added product skeleton for adding items hardcoded

---
+++ src/components/SearchResults/results.js
@@ -0,0 +1,21 @@
+/**
+ * 
+  let product: {
+    product_id: number
+    alsoBought: [{other product id,number of times bought together}]
+    title: string
+    price: number
+    pictures: url string
+    rating: number
+    information: string
+  }
+
+  purpose of alsoBought is so that 'also bought together' 
+  can be shown for when items are bought more than once together
+
+
+  There are more properties of amazon items but I feel like this is enough for now.
+ * 
+ * 
+ * 
+ */
