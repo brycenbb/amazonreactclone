9f9fee13f482c7bd75f0b6acac6e8c00330efcf4
brycen <brycensoc@gmail.com>
2022-07-25 11:50:25 +0100

    commented out useEffect to save API calls

--- src/components/ProductCard/index.js
+++ src/components/ProductCard/index.js
@@ -1,3 +1,12 @@
-function ProductCard({ title, price, rating, pictures }) {}
+function ProductCard({ title, price, rating, pictures }) {
+  return (
+    <div className="productCard">
+      <div>{title}</div>
+      <div>${price}</div>
+      <div>Rating out of 5: {rating}</div>
+      <img src={pictures[0]} alt="product"></img>
+    </div>
+  );
+}
 
 export default ProductCard;
